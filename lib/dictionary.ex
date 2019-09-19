bca09dd69ee787177f58f9f77ec56453c234fb71
Stephen Gladney <stephengladney@gmail.com>
2019-09-19 00:45:49 -0400

    make first commit, return random word

---
+++ lib/dictionary.ex
@@ -0,0 +1,9 @@
+defmodule Dictionary do
+  def random_word() do
+    Enum.random(word_list())
+  end
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents, ~r/\n/)
+  end
+end
