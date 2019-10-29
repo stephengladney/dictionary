6c4e6ee14c956ac8ab3bb6da43477452b62d0df5
Stephen Gladney <stephengladney@gmail.com>
2019-10-28 23:22:37 -0400

    Hello pipe operator. Nice to meet you.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,9 +1,13 @@
 defmodule Dictionary do
-  def random_word() do
-    Enum.random(word_list())
+  def random_word do
+    word_list()
+    |> Enum.random()
   end
+
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list = String.split(contents, ~r/\n/)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
+
 end
